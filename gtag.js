124a134841966bf3be3112c74342dda326fe6d46
HAN ZHANG <44986061+johan508@users.noreply.github.com>
2025-02-02 18:19:32 -0500

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,10 @@
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-P9JHZPCK6Q"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+		
+		  gtag('config', 'G-P9JHZPCK6Q');
+		</script>

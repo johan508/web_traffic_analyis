aa298f6dabc3fce9718652d5bcadc27bdbe833e7
HAN ZHANG <44986061+johan508@users.noreply.github.com>
2025-02-12 14:08:19 -0500

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,10 +1,10 @@
 
 		<!-- Google tag (gtag.js) -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-P9JHZPCK6Q"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-P9*******"></script>
 		<script>
 		  window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 		
-		  gtag('config', 'G-P9JHZPCK6Q');
+		  gtag('config', 'G-P9*******');
 		</script>
